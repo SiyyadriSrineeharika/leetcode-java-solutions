8fcc0441915eec36e5af1c112211e12b4348037d
SiyyadriSrineeharika <srineeharika33@gmail.com>
2026-05-29 00:02:07 +0530

    Added better solution for two sum

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -12,4 +12,31 @@ class Solution {
         }
         return new int[] {};
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        // Better solution
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+
+        // keep in mind that given is unsorted array
+        // two pointer works well with sorted array only
+
+        // think like:
+        // target - nums[i] = complement
+        // 9 - 2 = 7
+
+        // Can I quickly check if 7 already exists?
+
+        // recognize the hashmap pattern here
+        // lookup is O(1)
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            int complement = target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{i,map.get(complement)};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[] {};
+    }
 }
